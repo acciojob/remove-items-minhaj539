c6617089ea6877401a934ef934ca121af31f954c
minhaj539 <115364661+minhaj539@users.noreply.github.com>
2023-04-26 13:56:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your JS code here. If required.
+let sel=document.getElementById("colorSelect")
+let btn=document.getElementsByTagName("input")[0];
+
+// 
+ btn.addEventListener("click",remove);
+
+
+function remove(){
+
+    let item=getSelected();
+    
+    for(let i=0;i<sel.length;i++){
+        let child=sel[i];
+        if(child.value==item){
+            sel.removeChild(child);
+            break;
+        }
+    }
+}
+
+let getSelected=function(){
+	if(sel.length==0) return;
+    let item=document.getElementById("colorSelect").value;
+    return item;
+}
+let item=sel.addEventListener("change", getSelected);
